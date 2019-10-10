4a71bee5653cb3fcdc64dcc3cefc3755433c478b
Martinez-coder <programacion.libre.ada@gmail.com>
2019-10-10 13:27:55 -0500

    mi primer commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let cantidadDeDolares = 500
+
+let tasaDeCambio = 19
+
+let cantidadPesosAAhorrar = cantidadDeDolares * tasaDeCambio
+
+console.log('la cantidad que Fulanito tiene que ahorrar:',cantidadPesosAAhorrar)
